c1e7716ab8572a942960e9f0378165f9642d3f0a
Pascal Wegner <p.wegner1992@gmail.com>
2020-03-06 23:48:10 +0100

    Implemented login request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { ChangeEvent } from "react";
+import { Input, Button } from "antd";
+import "antd/dist/antd.css";
 function App() {
-    return <div className="App">Client works!</div>;
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const handleChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+    const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+    const login = () => {
+        fetch("http://localhost:8080/login", {
+            method: "POST",
+            mode: "no-cors",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                username,
+                password
+            })
+        });
+    };
+    return (
+        <div className="App">
+            <Input size="large" placeholder="Username" value={username} onChange={handleChangeUsername} />
+            <Input.Password size="large" placeholder="Password" value={password} onChange={handleChangePassword} />
+            <Button type="primary" onClick={login}>
+                Login
+            </Button>
+        </div>
+    );
 }
 
 export default App;
